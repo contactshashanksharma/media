d781c5e28152f05ca2dcc2193d957baf8fe7fda3
Shashank Sharma <shashank.sharma@intel.com>
2018-09-11 11:17:35 +0530

    something
    Signed-off-by: Shashank Sharma <shashank.sharma@intel.com>

--- FFmpeg/build_ffm_sample.sh
+++
@@ -1,2 +0,0 @@
-rm -rf ffmpeg-sample
-gcc -o ffmpeg-sample ffmpeg-sample.c -g -lavformat -lavcodec -lswresample -lswscale -lavutil -lm -lz -lpthread
--- FFmpeg/command_make_sample.sh
+++
@@ -1,1 +0,0 @@
-gcc -o ffmpeg-sample ffmpeg-sample.c -g -lavformat -lavcodec -lswresample -lswscale -lavutil -lm -lz -lpthread -I/home/shashanks/code/media/FFmpeg
